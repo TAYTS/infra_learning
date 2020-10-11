e3eb1890c38e724279d7029647c251d0af0ab4a2
tzushieh_tay <tzushieh_tay@mymail.sutd.edu.sg>
2020-10-11 18:04:26 +0800

    Integrate variable file value and add output of the EIP

--- WEEK2/learn-terraform-aws-instance/example.tf
+++ WEEK2/learn-terraform-aws-instance/example.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "ap-southeast-1"
+  region  = var.region
 }
 
 resource "aws_instance" "example" {
@@ -17,3 +17,11 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 }
 
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
